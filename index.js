0da7c1bfee1c1206f11e4f0dfdeef803623e3861
Eric-Martin <gaspar.ericmartin@gmail.com>
2023-07-10 13:57:44 -0400

    Add event listeners for landing page buttons

---
+++ index.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('fish-button').addEventListener('click', createCards)
+    document.getElementById('bug-button').addEventListener('click', createCards)
+})
+
